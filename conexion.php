5806203ccb090d42015f6c434f77c5803b1cf2b9
Carherre <jcherrerap@libertadores.edu.co>
2022-09-18 18:39:26 -0500

    information connection updated

--- conexion.php
+++ conexion.php
@@ -1,11 +1,21 @@
 <?php
 
-$con = mysqli_init(); 
-mysqli_ssl_set($con,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", 
-NULL, NULL); 
-mysqli_real_connect($conn, "mesaayuda.mysql.database.azure.com", "bdmesaayuda", 
-"C4r0l1n42020*", "mesaayuda", 3306, MYSQLI_CLIENT_SSL);
-	
-if (mysqli_connect_errno($conn)) {
-	die('Failed to connect to MySQL: '.mysqli_connect_error());
+$host = 'mesaayuda.mysql.database.azure.com';
+$username = 'bdmesaayuda@mesaayuda';
+$password = 'C4r0l1n42020*';
+$db_name = 'mesaayuda';
+
+//Initializes MySQLi
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, 'mesaayuda.mysql.database.azure.com', 'bdmesaayuda@mesaayuda', 'C4r0l1n42020*', 'mesaayuda', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 ?>
