afa001acfbc3bbe96d4298e9a3baae62bd3023cc
Carherre <jcherrerap@libertadores.edu.co>
2022-09-18 18:13:45 -0500

    updated connection

--- conexion.php
+++ conexion.php
@@ -1,6 +1,8 @@
 <?php
 
-$con = mysqli_init(); mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL); mysqli_real_connect($conn, "mesaayuda.mysql.database.azure.com", "bdmesaayuda", "{your_password}", "{your_database}", 3306, MYSQLI_CLIENT_SSL);
+$con = mysqli_init(); mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", 
+NULL, NULL); mysqli_real_connect($conn, "mesaayuda.mysql.database.azure.com", "bdmesaayuda", 
+"C4r0l1n42020*", "mesaayuda", 3306, MYSQLI_CLIENT_SSL);
 	
 	if($conn->connect_errno)
 	{
