2fd42ce8f9f4e1b310a9803759a8ccc6331bd7de
Carherre <jcherrerap@libertadores.edu.co>
2022-09-18 20:03:53 -0500

    test

--- conexion.php
+++ conexion.php
@@ -1,8 +1,28 @@
-
 <?php
-$con = mysqli_init(); 
-mysqli_ssl_set($con,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "mesaayuda.mysql.database.azure.com", "bdmesaayuda", "C4r0l1n42020*", "it", 3306, MYSQLI_CLIENT_SSL);
+	//Sintaxis de conexión de la base de datos de muestra para PHP y MySQL.
+	
+	//Conectar a la base de datos
+	
+	$hostname="mesaayuda.mysql.database.azure.com";
+	$username="bdmesaayuda";
+	$password="C4r0l1n42020*";
+	$dbname="it";
+	$usertable="Ingreso";
+	
+	mysqli_connect($hostname,$username, $password) o morir ("html>script language='JavaScript'>alert('¡No es posible conectarse a la base de datos! Vuelve a intentarlo más tarde.'),history.go(-1)/script>/html>");
+	mysqli_select_db($dbname);
+	
+	# Comprobar si existe registro
+	
+	$query = “SELECCIONAR * DESDE $usertable”;
+	
+	$result = mysqli_query($query);
+	
+	si($result){
+		while($row = mysqli_fetch_array($result)){
+			$name = $row["$yourfield"];
+			echo "Nombre: ".$name."br/>";
+		}
+	}
 ?>
-
 
