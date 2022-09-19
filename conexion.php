328df922b250b88bd7c9e479e8f08c4b97d714a7
Carherre <jcherrerap@libertadores.edu.co>
2022-09-18 19:50:59 -0500

    test

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 <?php
 $con = mysqli_init(); 
 mysqli_ssl_set($con,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "mesaayuda.mysql.database.azure.com", "bdmesaayuda", "C4r0l1n42020*", "mesaayuda", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, "mesaayuda.mysql.database.azure.com", "bdmesaayuda", "C4r0l1n42020*", "it", 3306, MYSQLI_CLIENT_SSL);
 ?>
 
 
--- login.php
+++ login.php
@@ -4,14 +4,11 @@
 
 $usuario = $_POST["txtusuario"];
 $pass 	= $_POST["txtpassword"];
-$rol 	= $_POST["rol"];
 
-
-
-$queryusuario = mysqli_query($conn,"SELECT * FROM login WHERE usuario ='$usuario' and pass = '$pass' and rol = '$rol'");
+$queryusuario = mysqli_query($conn,"SELECT * FROM login WHERE usuario ='$usuario' and pass = '$pass'");
 $nr 		= mysqli_num_rows($queryusuario);  
 	
-if ($nr == 1 )  
+/*if ($nr == 1 )  
 	{ 
 		if($rol=="Usuario")
 			{	
@@ -25,6 +22,6 @@ if ($nr == 1 )
 	else
 	{
 	echo "<script> alert('Usuario, contraseña o rol incorrecto.');window.location= 'index.html' </script>";
-	}
+	}*/
  
 ?>
