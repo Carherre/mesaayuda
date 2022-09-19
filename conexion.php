e1ad41bc9e106a8d4dd810959381cd829a0d09c2
Carherre <jcherrerap@libertadores.edu.co>
2022-09-18 19:38:22 -0500

    test1

--- conexion.php
+++ conexion.php
@@ -1,11 +1,8 @@
 
 <?php
-$conn = new mysqli("mesaayuda.mysql.database.azure.com","bdmesaayuda","C4r0l1n42020*","mesaayuda");
-	
-	if($conn->connect_errno)
-	{
-		echo "No hay conexión: (" . $conn->connect_errno . ") " . $conn->connect_error;
-	}
+$con = mysqli_init(); 
+mysqli_ssl_set($con,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, "mesaayuda.mysql.database.azure.com", "bdmesaayuda", "C4r0l1n42020*", "mesaayuda", 3306, MYSQLI_CLIENT_SSL);
 ?>
 
 
