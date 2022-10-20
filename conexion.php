8a3e12de63ec7427c189a7fe1f3642612ec8e002
Carherre <jcherrerap@libertadores.edu.co>
2022-10-20 10:51:41 -0500

    conexion modified

--- conexion.php
+++ conexion.php
@@ -1,9 +1,43 @@
 <?php
 
-$conn = new mysqli("localhost","root","","mesa_ayuda");
-	
-	if($conn->connect_errno)
-	{
-		echo "No hay conexión: (" . $conn->connect_errno . ") " . $conn->connect_error;
-	}
-?>
+  $servername = "bdmesaayuda.mysql.database.azure.com";
+
+  $database = "mesa_ayuda";
+
+  $username = "bdmesaayuda";
+
+  $password = "C4r0l1n42020*";
+
+   // Create connection
+
+  $conn = mysqli_connect($servername, $username, $password, $database);
+
+   // Check connection
+
+   if (!$conn) {
+
+     die("Connection failed: " . mysqli_connect_error());
+
+  }
+
+    echo "Connected successfully";
+    
+    $resultado = mysqli_query($conn);
+
+     if($resultado) {
+
+      echo "Registro agregado";
+
+    }
+
+     else{
+
+      echo "Error en Registro";
+
+    }
+
+    mysqli_close($conn);
+
+
+
+ ?>
